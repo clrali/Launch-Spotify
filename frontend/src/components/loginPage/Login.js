3fe17ff78f13443d5b039db13400bd6b1c4ff6cf
Luke Xue <luqi9900@gmail.com>
2022-05-31 14:51:25 -0600

    confused no api key

--- frontend/src/components/loginPage/Login.js
+++ frontend/src/components/loginPage/Login.js
@@ -1,12 +1,40 @@
-import Typography from '@mui/material/Typography';
+import {
+  Typography,
+  Card,
+  CardContent,
+  Box,
+  Grid,
+  Button,
+} from "@mui/material";
 
+const Login = (props) => {
+  function handleClick(e) {
+    e.preventDefault();
+  }
 
-const Login = props => {
-	return (
-		<>
-			<Typography variant="h4">Login page!</Typography>
-		</>
-	);
+  return (
+    <div style={{ backgroundColor: "blue", height: "100vh", marginTop: 0, position: 'fixed', width: '100vw' }}>
+      <div>
+        <Card
+          sx={{
+            variant: "outlined",
+            boxShadow: 7,
+            maxWidth: 400,
+            minHeight: 100,
+            justifyContent: "center",
+            margin: "auto",
+            marginTop: "30vh",
+          }}
+        >
+          <CardContent>
+            <Button variant="contained" onClick={handleClick}>
+              Login
+            </Button>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
