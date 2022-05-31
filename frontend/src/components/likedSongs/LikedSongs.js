45d4ff2ccc17b9fcd8eee1c7d6a13b80f22b9564
clara li <clara4695@gmail.com>
2022-05-31 15:00:53 -0400

    changed components

--- frontend/src/components/likedSongs/LikedSongs.js
+++ frontend/src/components/likedSongs/LikedSongs.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function LikedSongs() {
-  return (
-    <div>
-        <h1>
-            Liked Songs
-        </h1>
-    </div>
-  )
+    const url = "http://localhost:9000/spotify"
+
+    useEffect(() => {
+        fetch(url + "/profile")
+        .then((res) => res.json())
+        .then((data) => {
+        })
+        .catch((err) => console.log(err))
+    }, [])
+
+    return (
+        <div>
+            <h1>
+                Liked Songs
+            </h1>
+        </div>
+    )
 }
 
 export default LikedSongs
