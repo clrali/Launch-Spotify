ec76640a2935c0bbd423fe16d5b6cc023258f5ec
Keshav Bhateja <keshavbhateja1@gmail.com>
2022-06-01 02:29:47 +0530

    changed the background

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,21 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar.js";
 import { Outlet, useLocation } from "react-router-dom";
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
+    <div className="background--custom">
     <div className="App">
+      
       <Navbar />
       <Outlet />
       {useLocation().pathname === "/"}
+      
     </div>
+    </div>
+
+
   );
 }
 
