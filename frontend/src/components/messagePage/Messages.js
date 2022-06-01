98f7673e629f8475aac80f4f6be4f15b2e81c488
Matthew Haid <wsd6vn@virginia.edu>
2022-06-01 13:57:04 -0400

    layout for messaging

--- frontend/src/components/messagePage/Messages.js
+++ frontend/src/components/messagePage/Messages.js
@@ -1,21 +1,93 @@
 import Typography from "@mui/material/Typography";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import { Button, AppBar, Box, IconButton, Toolbar, Stack, Card } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import {
+  Button,
+  AppBar,
+  Box,
+  IconButton,
+  Toolbar,
+  Stack,
+  Card,
+  InputBase,
+  Paper,
+  Grid,
+} from "@mui/material";
 
 const Messages = (props) => {
   return (
     <>
       <AppBar color="primary">
-		  <Box>
-          <IconButton size='large' aria-label="back" variant="contained">
+        <Box>
+          <IconButton size="large" aria-label="back" variant="contained">
             <ArrowBackIosIcon color="secondary" />
           </IconButton>
-          <IconButton size='large' aria-label="back" variant="contained">
+          <IconButton size="large" aria-label="back" variant="contained">
             <ArrowForwardIosIcon color="secondary" />
           </IconButton>
-		  </Box>
+        </Box>
       </AppBar>
+
+      <Card
+        sx={{
+          bgcolor: "white",
+          marginTop: "3%",
+          marginLeft: "30%",
+          marginRight: "25%",
+          paddingBottom: "18%",
+          boxShadow: 7,
+          borderRadius: 2,
+        }}
+      >
+        <Grid container>
+          <Grid item xs={6}>
+            <Paper
+              component="form"
+              sx={{
+                p: "2px 4px",
+                display: "flex",
+                alignItems: "center",
+                width: 400,
+              }}
+            >
+              <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder="Search Mesengers"
+                inputProps={{ "aria-label": "search person" }}
+              />
+              <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
+                <SearchIcon />
+              </IconButton>
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography> User </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Stack>
+              <Button variant="text"> User1 </Button>
+              <Button variant="text"> User2 </Button>
+              <Button variant="text"> User3 </Button>
+              <Button variant="text"> User4 </Button>
+            </Stack>
+          </Grid>
+          <Grid item xs={6}>
+            <Box>
+              <Typography variant="h6"> User </Typography>
+              <Typography variant="p"> message </Typography>
+            </Box>
+            <Box>
+              <Typography variant="h6"> User </Typography>
+              <Typography variant="p"> message </Typography>
+            </Box>
+            <Box>
+              <Typography variant="h6"> User </Typography>
+              <Typography variant="p"> message </Typography>
+            </Box>
+          </Grid>
+        </Grid>
+      </Card>
     </>
   );
 };
