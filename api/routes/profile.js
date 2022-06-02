1641d938bb682866778f8ce3f566bcabcf6c4662
Matthew Haid <wsd6vn@virginia.edu>
2022-06-02 17:18:51 -0400

    spotify data

--- api/routes/profile.js
+++ api/routes/profile.js
@@ -24,25 +24,6 @@ router.get("/info", async (req, res, next) => {
   res.json({ result: allDocData });
 });
 
-router.post("/userLiked", async (req, res, next) => {
-  const newRef = await doc(collection(db, "profile"));
-  await setDoc(newRef, { user: req.user });
-  const url = "https://api.spotify.com/v1/me/tracks?offset=0&limit=10";
-  const data = await fetch(url, {
-    headers: {
-      Authorization: "Bearer " + req.query.token,
-    },
-  })
-    .catch((err) => console.log(err))
-    .then((res) => res.json())
-    .then((data) =>
-      data.items.map((val, key) => {
-        setDoc(doc("profile", "", "likedSongs"), {
-          title: val.track.name,
-        });
-      })
-    );
-  return "hello!";
-});
+
 
 module.exports = router;
