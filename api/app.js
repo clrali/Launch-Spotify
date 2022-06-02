7b12dd59378bb5669b0bd1b68ea14c41a8f5bfa4
Keshav Bhateja <keshavbhateja1@gmail.com>
2022-06-03 00:08:27 +0530

    Deploy

--- api/app.js
+++ api/app.js
@@ -34,6 +34,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/spotify', spotifyRouter);
 app.use('/profile', profileRouter);
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
