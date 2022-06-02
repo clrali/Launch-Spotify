42932d3d0b4ae422bd2df70ba61ab77a5f22fba9
Luke Xue <luqi9900@gmail.com>
2022-06-02 09:03:34 -0600

    extras

--- frontend/src/components/likedSongs/LikedSongs.js
+++ frontend/src/components/likedSongs/LikedSongs.js
@@ -14,13 +14,8 @@ import { AccessTokenContext } from '../../Contexts/accessTokenContext';
 import { useContext } from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
-import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import CardMedia from '@mui/material/CardMedia';
-import Box from '@mui/material/Box'
 
 
 function LikedSongs() {
