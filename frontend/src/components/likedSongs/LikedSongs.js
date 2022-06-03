744fd486408f663fcab1a537ff2217b691438169
Keshav Bhateja <keshavbhateja1@gmail.com>
2022-06-03 19:09:02 +0530

    resolved conflicts

--- frontend/src/components/likedSongs/LikedSongs.js
+++ frontend/src/components/likedSongs/LikedSongs.js
@@ -1,109 +1,66 @@
-
-// import {
-//     Typography,
-//     Card,
-//     CardContent,
-//     Box,
-//     Grid,
-//     Button,
-// } from "@mui/material";
-// import { Container } from "react-bootstrap"
-// import "bootstrap/dist/css/bootstrap.css"
-
-// import React from 'react'
-// import { AccessTokenContext } from '../../Contexts/accessTokenContext';
-// import { useContext } from 'react';
-// import { useState } from 'react';
-// import { useEffect } from 'react';
-// import {Helmet} from "react-helmet";
-
-
-// function LikedSongs() {
-    
-//     const { accessToken } = useContext(AccessTokenContext);
-//     const [songs, setSongs] = useState([])
-
-//     useEffect(() => {
-//         fetch("http://localhost:9000/users?token=" + accessToken)
-//         .then(res => res.json())
-//         .then(data => {
-//             setSongs(data.items)
-//         })
-
-//     }, [])
-   
-//     console.log(songs)
-//     return (
-
-        
-
-//         <div>
-//             <Helmet>
-//             <title>Liked Songs</title>
-//             </Helmet>
-//             <h1>Liked Songs</h1>
-//             {songs.length > 0 && 
-//                 songs.map((val, key) => {
-//                     return <p>{val.track.name} by {val.track.artists[0].name}</p>
-//             })
-//             }
-
-//         </div>
-        
-//     )
-
-import { Typography, Card, CardContent, Box, Button } from "@mui/material";
-import React from "react";
-import { AccessTokenContext } from "../../Contexts/accessTokenContext";
-import { useContext } from "react";
-import { useState } from "react";
-import { useEffect } from "react";
-import CardActions from "@mui/material/CardActions";
-import { Link, Outlet } from "react-router-dom"
-import "./LikedSongs.css";
+import {
+  Typography,
+  Card,
+  CardContent,
+  Box,
+  Button,
+} from "@mui/material";
+import React from 'react'
+import { AccessTokenContext } from '../../Contexts/accessTokenContext';
+import { useContext } from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import CardActions from '@mui/material/CardActions';
 
 function LikedSongs() {
-    
+  const { accessToken } = useContext(AccessTokenContext);
+  const [songs, setSongs] = useState([])
+
+  useEffect(() => {
+      fetch("http://localhost:9000/users/likedsongs?token=" + accessToken)
+      .then(res => res.json())
+      .then(data => {
+          setSongs(data.items)
+          console.log(data)
+      })
+
+  }, [])
+  
   return (
-    <div>
-      <h1>Liked Songs</h1>
-      <Button
-        component={Link}
-        to="/liked/month"
-        variant="outlined"
-        style={{
-          borderRadius: 35,
-          backgroundColor: "#FFFFFF",
-        }}
-      >
-        Past Month
-      </Button>
-
-      <Button
-        component={Link}
-        to="/liked/year"
-        variant="outlined"
-        style={{
-          borderRadius: 35,
-          backgroundColor: "#FFFFFF",
-        }}
-      >
-        Past Year
-      </Button>
-
-      <Button
-        component={Link}
-        to="/liked/all"
-        variant="outlined"
-        style={{
-          borderRadius: 35,
-          backgroundColor: "#FFFFFF",
-        }}
-      >
-        All Time
-      </Button>
-    </div>
-  );
-
+      <div>
+          <h1>Liked Songs</h1>
+          {songs.length > 0 && 
+              songs.map((val, key) => {
+                  return (
+                      <Box sx={{ mx: "auto", width: 500, p: 2 }} >
+                      <Card variant="outlined" style={{margin: '0 auto', display: "flex"}}>
+                          <img src={val.track.album.images[0].url} height="300" width="300"
+                              alt="album cover"
+                          />
+                          <div>
+                          <CardContent>
+                              <Typography sx={{ fontSize: 18 }} color="text.primary" gutterBottom>
+                                  {val.track.name} by {val.track.artists[0].name}
+                              </Typography>
+                          </CardContent>
+                          <CardActions>
+                              <Button 
+                              size="small" 
+                              href={val.track.preview_url} 
+                              target="_blank"
+                              variant="outlined"
+                              style={{margin: '0 auto', display: "flex"}}>
+                                  Preview Song
+                              </Button>
+                          </CardActions>
+                          </div>
+                      </Card>
+                      </Box>
+                  )
+          })
+          }
+      </div>
+  )
 }
+
 export default LikedSongs;
